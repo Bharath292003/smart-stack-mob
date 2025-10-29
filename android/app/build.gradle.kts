c24fc5cae97d819ef3ad8d4637acea5bfe590e90
Rahul Verma <rahul.verma@almond.solutions>
2025-10-29 12:37:37 +0530

    Changes according to app crashes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.smart_stack"
+    namespace = "com.smart_stack.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,12 +16,13 @@ android {
     }
 
     kotlinOptions {
+
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.smart_stack"
+        applicationId = "com.smart_stack.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/smart_stack/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.smart_stack
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
